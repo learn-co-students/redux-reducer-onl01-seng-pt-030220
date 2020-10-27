cabff214d70fdb934d403b929d285fdf42cf4712
Rkebede <rutht.kebede@gmail.com>
2020-10-27 14:18:19 -0500

    Done.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,10 @@
 export function managePresents(state, action){
+  state = {
+    numberOfPresents: 0
+  }
+   if (action.type === 'INCREASE'){
+     return {numberOfPresents: state.numberOfPresents +1}
+   }else{
+     return state
+   }
 }
