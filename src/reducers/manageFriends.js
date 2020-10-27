4a35026eb3a14bbb5fb908da59608c12c146da34
Clannis <Dodds1990@gmail.com>
2020-10-27 06:01:01 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,12 +1,3 @@
-// const action = {
-//     type: "ADD_FRIEND",
-//     friend: {
-//       name: "Chrome Boi",
-//       homewtown: "NYC",
-//       id: 1
-//     }
-//   }
-  
 export function manageFriends(state = { friends: [] }, action){
     switch (action.type) {
         case "ADD_FRIEND":
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,7 +1,3 @@
-const action = {
-    type:"INCREASE"
-}
-
 export function managePresents(state = {numberOfPresents: 0}, action){
     switch (action.type) {
         case "INCREASE":
