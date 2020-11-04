94e2da34a0db703e0901da7e7d739d575303d110
engahmed10 <eng.computer1@yahoo.com>
2020-11-04 14:49:38 -0800

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -4,7 +4,7 @@ export function manageFriends(state, action){
   switch(action.type){
 
      case 'ADD_FRIEND':
-       return {friends: state.friends.push({ name: 'Joe', hometown: 'Boston', id: 101 }),...state }
+       return {friends: state.friends.push(action.friend),...state }
        case 'REMOVE_FRIEND':
          
          return {...state,friends: state.friends.filter(friend=> parseInt(friend.id) !== parseInt(action.id)) }
