67d0c268808c7fbadfe8fff501a2f657f3b2ea2b
SincerelyBrittany <sincerelybr@gmail.com>
2020-11-14 20:04:20 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,44 @@
-export function manageFriends(state, action){
-}
+// export function manageFriends(state, action){
+//     let state = {friends: []}
+//     let action = {
+//         type: "ADD_FRIEND",
+//         friend: {
+//           name: "Chrome Boi",
+//           hometown: "NYC",
+//           id: 1
+//         }
+//       }
+// }
+
+
+export function manageFriends(state = {
+    friends: [],
+  }, action) {
+    switch(action.type) {
+  
+      case "ADD_FRIEND":
+        return (
+          {...state,
+              friends: [
+                ...state.friends,
+                action.friend
+              ]
+          }
+      )
+  
+      case "REMOVE_FRIEND":
+        const removalIndex = state.friends.findIndex(friend => friend.id === action.id);
+        return (
+          {...state,
+              friends: [
+                ...state.friends.slice(0, removalIndex),
+                ...state.friends.slice(removalIndex + 1)
+              ]
+          }
+        )
+  
+      default:
+        return state;
+  
+    }
+  };
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,32 @@
-export function managePresents(state, action){
-}
+// export function managePresents(state, action){
+//     let state = {numberOfPresents: 0}
+//     let action = {type: 'INCREASE'}
+
+//     function changeState(state, action){
+//         switch (action.type) {
+//           case 'INCREASE':
+//             return {numberOfPresents: state.numberOfPresents + 1}
+//           default:
+//             return state;
+//         }
+//       }
+
+//       changeState(state, action)
+
+// }
+
+
+export function managePresents(state = {
+    numberOfPresents: 0,
+  }, action) {
+    switch(action.type) {
+  
+      case "INCREASE":
+        return Object.assign({}, state, {
+          numberOfPresents: state.numberOfPresents + 1
+        });
+  
+      default:
+        return state;
+    }
+  };
